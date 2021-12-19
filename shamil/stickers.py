f1287640ebdca6a2417dbdc629cdf08d3dd595dd
DavidCartoons-1 <70798117+DavidCartoons-1@users.noreply.github.com>
2021-12-19 15:11:26 +0530

    Update stickers.py

--- shamil/stickers.py
+++ shamil/stickers.py
@@ -12,6 +12,6 @@ from pyrogram.types import User, Message, Sticker, Document
 @Client.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+       await message.reply(f"**Sticker ID :**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID : ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
-       await message.reply("Oops !! Not a sticker file")
+       await message.reply("Not A Sticker File")
