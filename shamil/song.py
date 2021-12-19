64b912cc94b972fb19c662d653e6d2339ed85aaf
DavidCartoons-1 <70798117+DavidCartoons-1@users.noreply.github.com>
2021-12-19 15:14:34 +0530

    Update song.py

--- shamil/song.py
+++ shamil/song.py
@@ -22,7 +22,7 @@ def a(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('`Searching... Please Wait...`')
+    m = message.reply('**Searching...⏳**')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
@@ -53,21 +53,21 @@ def a(client, message):
 
         except Exception as e:
             print(e)
-            m.edit('**👎 Nᴏᴛʜɪɴɢ Tᴏ Bᴇ Fᴏᴜɴᴅ 🥺 Oʀ Cʜᴇᴄᴋ Sᴩᴇʟʟɪɴɢ 🤗!**')
+            m.edit('**Nᴏᴛʜɪɴɢ Tᴏ Bᴇ Fᴏᴜɴᴅ Oʀ Cʜᴇᴄᴋ Sᴩᴇʟʟɪɴɢ**')
             return
     except Exception as e:
         m.edit(
-            "**Enter Song Name with /song Command!**"
+            "**Enter Song Name With /song Command**"
         )
         print(str(e))
         return
-    m.edit("`Bruh... Uploading... Please Wait...`")
+    m.edit("**Uploading ⬆️**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>Title:</b> <a href="{link}">{title}</a>\n⌚ <b>Duration:</b> <code>{duration}</code>\n📻 <b>Uploaded By:</b> <a href="https://t.me/joinchat/OsJr6i6C05E0NmQ1">[MwK] Musics</a>'
+        rep = f'<b>Title :</b> <a href="{link}">{title}</a>\n <b>Duration :</b> <code>{duration}</code>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -76,7 +76,7 @@ def a(client, message):
         m.delete()
         message.delete()
     except Exception as e:
-        m.edit('**Sᴇᴇᴍꜱ Lɪᴋᴇ Aɴ Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ 🥶 Report This @redbullfed!!**')
+        m.edit('**Sᴇᴇᴍꜱ Lɪᴋᴇ Aɴ Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ Report This**')
         print(e)
     try:
         os.remove(audio_file)
